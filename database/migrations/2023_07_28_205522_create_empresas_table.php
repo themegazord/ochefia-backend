2829c724e72f6a8602b16ff334b3e5e63397e1d4
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-07-28 16:59:56 -0400

    feat: criado a migration da tabela empresas

---
+++ database/migrations/2023_07_28_205522_create_empresas_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('empresas', function (Blueprint $table) {
+            $table->id('empresa_id');
+            $table->string('empresa_nome', 255);
+            $table->string('empresa_cnpj', 14)->unique();
+            $table->string('empresa_descricao', 30);
+            $table->string('empresa_logo');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('empresas');
+    }
+};
