309795ecf5979f6ee0a7613a99d45f5a2ffeec58
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-03 10:59:30 -0400

    chore: criado respostas padroes para parametros integer, array e exists

--- app/Http/Requests/RequestPadroes.php
+++ app/Http/Requests/RequestPadroes.php
@@ -7,6 +7,8 @@ class RequestPadroes {
     public static $required = 'Esse campo é obrigatório, por favor, informe-o.';
     public static $email = 'O email informado é inválido.';
     public static $image = 'O campo deve receber apenas imagens.';
+    public static $integer = 'O campo deve receber apenas inteiros.';
+    public static $array = 'O campo deve receber apenas arrays.';
 
     public static function mensagemMax(int $valorMaximo): string {
         return 'Esse campo tem que conter no máximo ' . $valorMaximo . ' caracteres.';
@@ -14,5 +16,9 @@ class RequestPadroes {
 
     public static function mensagemMimes(array $valorMimes): string {
         return 'A extensão do arquivo deve ser ' . implode(' ou ', $valorMimes) . '.';
+    }
+
+    public static function mensagemExists(string $tabela): string {
+        return 'O id não existe na tabela ' . $tabela . '.';
     }
 }
