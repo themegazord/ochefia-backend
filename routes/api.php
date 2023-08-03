8f6eeac9a4d4b052c88b2d61724b8d1e6e07a326
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-03 16:39:57 -0400

    chore: adicionado a rota para cadastro de funcionários

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\AutenticacaoController;
 use App\Http\Controllers\EmpresaController;
 use App\Http\Controllers\EnderecoController;
+use App\Http\Controllers\FuncionarioController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -26,6 +27,9 @@ Route::prefix('v1')->group(function() {
         });
         Route::prefix('endereco')->group(function() {
             Route::post('cadastro', [EnderecoController::class, 'store']);
+        });
+        Route::prefix('funcionario')->group(function() {
+            Route::post('cadastro', [FuncionarioController::class, 'store']);
         });
     });
 });
