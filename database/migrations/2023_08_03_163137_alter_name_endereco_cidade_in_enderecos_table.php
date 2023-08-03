127a9e930a42666f9a58f41274ca9c6678ec5c22
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-03 12:34:22 -0400

    chore: renomeado o a coluna endereco_cidadae para endereco_cidade

---
+++ database/migrations/2023_08_03_163137_alter_name_endereco_cidade_in_enderecos_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('enderecos', function (Blueprint $table) {
+            $table->renameColumn('endereco_cidadae', 'endereco_cidade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('enderecos', function (Blueprint $table) {
+            $table->renameColumn('endereco_cidade', 'endereco_cidadae');
+        });
+    }
+};
