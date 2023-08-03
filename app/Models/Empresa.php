e638280178c663536aa6fe2719937654f79d5b9d
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-03 11:00:24 -0400

    chore: realizado a relacao hasone com o model funcionarios

--- app/Models/Empresa.php
+++ app/Models/Empresa.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Empresa extends Model
 {
@@ -15,4 +16,8 @@ class Empresa extends Model
         'empresa_descricao',
         'empresa_logo'
     ];
+
+    public function funcionario(): HasOne {
+        return $this->hasOne(Funcionario::class, 'empresa_id', 'empresa_id');
+    }
 }
