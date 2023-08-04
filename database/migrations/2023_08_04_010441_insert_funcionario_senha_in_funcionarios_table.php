100053df8a689c0ed57d6e78b8c978e7c911c902
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-03 21:06:28 -0400

    chore: adicionado a coluna funcionario_senha na tabela funcionarios

---
+++ database/migrations/2023_08_04_010441_insert_funcionario_senha_in_funcionarios_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('funcionarios', function (Blueprint $table) {
+            $table->string('funcionario_senha', 255)->after('funcionario_email');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('funcionarios', function (Blueprint $table) {
+            $table->dropColumn('funcionario_senha');
+        });
+    }
+};
