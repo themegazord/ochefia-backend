4241a3d8114dff4014e0050a55613e2a5f12002b
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-03 20:59:53 -0400

    chore: criado a migration que cria a chave estrangeira usuario_id na tabela funcionarios

---
+++ database/migrations/2023_08_04_005512_insert_user_relationship_in_funcionarios_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('funcionarios', function (Blueprint $table) {
+            $table->unsignedBigInteger('usuario_id')->after('funcionario_id');
+            $table->foreign('usuario_id')->references('usuario_id')->on('users');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('funcionarios', function (Blueprint $table) {
+            $table->dropForeign('funcionarios_usuario_id_foreign');
+            $table->dropColumn('usuario_id');
+        });
+    }
+};
