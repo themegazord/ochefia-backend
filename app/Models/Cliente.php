db80da442f36e14ef8a7dfb83c8903d376deb8c1
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-05 00:33:42 -0400

    chore: registrado no Model de clientes os relacionamentos com a tabela users e enderecos

---
+++ app/Models/Cliente.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Cliente extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'usuario_id',
+        'endereco_id',
+        'cliente_nome',
+        'cliente_email',
+        'cliente_senha',
+        'cliente_cpf',
+        'cliente_telefone',
+        'cliente_telefone_contato'
+    ];
+
+    protected $hidden = [
+        'cliente_senha'
+    ];
+
+    public function user(): BelongsTo {
+        return $this->belongsTo(User::class, 'usuario_id', 'id');
+    }
+
+    public function endereco(): BelongsTo {
+        return $this->belongsTo(Endereco::class, 'endereco_id', 'endereco_id');
+    }
+}
