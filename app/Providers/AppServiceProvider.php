61219cb53cb7533fe13aa13005a8d9863dbaecd7
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-05 00:38:11 -0400

    chore: registro o servico ClienteService e linkado a interface ICliente e o repositorio ClienteRepository

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,15 +4,18 @@ namespace App\Providers;
 
 use App\Actions\Endereco\ValidaDadosCEP;
 use App\Repositories\Eloquent\Autenticacao\UsuarioRepository;
+use App\Repositories\Eloquent\Cliente\ClienteRepository;
 use App\Repositories\Eloquent\Empresa\EmpresaRepository;
 use App\Repositories\Eloquent\Endereco\EnderecoRepository;
 use App\Repositories\Eloquent\Funcionario\FuncionarioRepository;
 use App\Repositories\Interfaces\Autenticacao\IUsuario;
+use App\Repositories\Interfaces\Cliente\ICliente;
 use App\Repositories\Interfaces\Empresa\IEmpresa;
 use App\Repositories\Interfaces\Endereco\IEndereco;
 use App\Repositories\Interfaces\Funcionarios\IFuncionario;
 use App\Services\Autenticacao\CadastroService;
 use App\Services\Autenticacao\LoginService;
+use App\Services\Cliente\ClienteService;
 use App\Services\Empresa\EmpresaService;
 use App\Services\Endereco\EnderecoService;
 use App\Services\Funcionario\FuncionarioService;
@@ -50,6 +53,12 @@ class AppServiceProvider extends ServiceProvider
             $empresaService = $app->make(EmpresaService::class);
             return new FuncionarioService($funcionarioRepository, $cadastroService, $loginService, $empresaService);
         });
+        $this->app->scoped(ClienteService::class, function (Application $app) {
+            $clienteRepository = $app->make(ICliente::class);
+            $cadastroService = $app->make(CadastroService::class);
+            $loginService = $app->make(LoginService::class);
+            return new ClienteService($clienteRepository, $cadastroService, $loginService);
+        });
     }
 
     /**
@@ -61,5 +70,6 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(IEmpresa::class, EmpresaRepository::class);
         $this->app->bind(IEndereco::class, EnderecoRepository::class);
         $this->app->bind(IFuncionario::class, FuncionarioRepository::class);
+        $this->app->bind(ICliente::class, ClienteRepository::class);
     }
 }
