8fe313d44d812f193c827ed70ae4f22bce9ad402
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-07 12:16:10 -0400

    chore: criado as exceções usadas para o grupo de produtos

---
+++ app/Exceptions/GrupoProdutoException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+
+class GrupoProdutoException extends Exception
+{
+    public static function TipoGrupoProdutoNaoExistente(string $tipo): self {
+        throw new self('O tipo de produto ' . $tipo . ' não existe no sistema, por favor, verificar na documentação', Response::HTTP_CONFLICT);
+    }
+}
