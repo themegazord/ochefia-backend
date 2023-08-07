04e0a85a04b5fb90cc7304eef5f1ffdd84b9722c
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-07 15:28:36 -0400

    chore: alterado o status do exceção de 409 - conflict para 404 - not found

--- app/Exceptions/GrupoProdutoException.php
+++ app/Exceptions/GrupoProdutoException.php
@@ -8,6 +8,6 @@ use Symfony\Component\HttpFoundation\Response;
 class GrupoProdutoException extends Exception
 {
     public static function tipoGrupoProdutoNaoExistente(string $tipo): self {
-        throw new self('O tipo de produto ' . $tipo . ' não existe no sistema, por favor, verificar na documentação', Response::HTTP_CONFLICT);
+        throw new self('O tipo de produto ' . $tipo . ' não existe no sistema, por favor, verificar na documentação', Response::HTTP_NOT_FOUND);
     }
 }
