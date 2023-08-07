eafb7f820f5e9aed310c811e13748a69230229d5
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-07 16:43:01 -0400

    feat: criado o form request para fazer a pré-validação de novos fornecedores de produtos

---
+++ app/Http/Requests/CadastroFornecedorProdutoRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CadastroFornecedorProdutoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'fornecedor_produto_nome' => 'required|string|max:90'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'required' => RequestPadroes::$required,
+            'string' => RequestPadroes::$string,
+            'fornecedor_produto_nome.max' => RequestPadroes::mensagemMax(90)
+        ];
+    }
+}
