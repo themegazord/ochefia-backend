8fd095f143f9b4ad4f959d316d08ee22a03c471a
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-07 18:58:26 -0400

    feat: criado o form request que vai pré validar os cadastros de novas unidades

---
+++ app/Http/Requests/CadastroUnidadeRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CadastroUnidadeRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'unidade_nome' => 'required|string|max:50'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'required' => RequestPadroes::$required,
+            'string' => RequestPadroes::$string,
+            'unidade_nome.max' => RequestPadroes::mensagemMax(50)
+        ];
+    }
+}
