5f8ff69c27285c59de2ad0002dd987218582835c
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-07 18:56:23 -0400

    feat: criado o service UnidadeService

---
+++ app/Services/Unidade/UnidadeService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services\Unidade;
+
+use App\Exceptions\UnidadeException;
+use App\Models\Unidade;
+use App\Repositories\Interfaces\Unidade\IUnidade;
+
+class UnidadeService
+{
+    public function __construct(
+        private readonly IUnidade $unidadeRepository
+    )
+    {
+    }
+
+    /**
+     * @throws UnidadeException
+     */
+    public function cadastro(array $unidade): Unidade|UnidadeException {
+        if ($this->consultaUnidadePeloNome($unidade['unidade_nome'])) return UnidadeException::unidadeDeMedidaJaExiste($unidade['unidade_nome']);
+        $unidade['unidade_nome'] = strtoupper($unidade['unidade_nome']);
+        return $this->unidadeRepository->cadastro($unidade);
+    }
+
+    private function consultaUnidadePeloNome(string $unidade_nome): ?Unidade {
+        return $this->unidadeRepository->unidadePorNome($unidade_nome);
+    }
+}
