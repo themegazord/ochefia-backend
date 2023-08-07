7fb7a2a416c240fb82778bc661a6f15101aa253f
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-07 14:24:51 -0400

    feat: criado o serviço SubGrupoProdutoService

---
+++ app/Services/SubGrupoProduto/SubGrupoProdutoService.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Services\SubGrupoProduto;
+
+use App\Exceptions\SubGrupoProdutoException;
+use App\Models\SubGrupoProduto;
+use App\Repositories\Interfaces\SubGrupoProduto\ISubGrupoProduto;
+
+class SubGrupoProdutoService
+{
+    public function __construct(
+        private readonly ISubGrupoProduto $subGrupoProdutoRepository
+    )
+    {
+    }
+
+    /**
+     * @throws SubGrupoProdutoException
+     */
+    public function cadastro(array $subGrupo):SubGrupoProduto|SubGrupoProdutoException {
+        if($this->consultaSubGrupoPorNome($subGrupo['sub_grupo_produto_nome'])) return SubGrupoProdutoException::subGrupoJaExistente($subGrupo['sub_grupo_produto_nome']);
+        $subGrupo['sub_grupo_produto_nome'] = strtoupper($subGrupo['sub_grupo_produto_nome']);
+        return $this->subGrupoProdutoRepository->cadastro($subGrupo);
+    }
+
+    private function consultaSubGrupoPorNome(string $nomeSubGrupo): ?SubGrupoProduto {
+        return $this->subGrupoProdutoRepository->subGrupoPorNome($nomeSubGrupo);
+    }
+}
