f346373d0a0e24f1917fe82a796c32c9c02ee453
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-08 09:56:27 -0400

    feat: criado o serviço ClasseProdutoService

---
+++ app/Services/ClasseProduto/ClasseProdutoService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Services\ClasseProduto;
+
+use App\Exceptions\ClasseProdutoException;
+use App\Models\ClasseProduto;
+use App\Repositories\Interfaces\ClasseProduto\IClasseProduto;
+
+class ClasseProdutoService
+{
+    public function __construct(
+        public readonly IClasseProduto $classeProdutoRepository
+    )
+    {
+    }
+
+    public function cadastro(array $classeProduto): ClasseProduto|ClasseProdutoException {
+        if ($this->consultaClasseProdutoPorNome($classeProduto['classe_produto_nome'])) return ClasseProdutoException::classeProdutoJaExiste($classeProduto['classe_produto_nome']);
+        $classeProduto['classe_produto_nome'] = strtoupper($classeProduto['classe_produto_nome']);
+        return $this->classeProdutoRepository->cadastro($classeProduto);
+    }
+
+    private function consultaClasseProdutoPorNome(string $nomeClasseProduto): ?ClasseProduto {
+        return $this->classeProdutoRepository->classeProdutoPorNome($nomeClasseProduto);
+    }
+}
