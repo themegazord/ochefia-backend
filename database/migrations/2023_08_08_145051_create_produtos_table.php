608f584ac92fd331c6601bababa8787743305238
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-08 11:42:45 -0400

    feat: criado tabela de produtos

---
+++ database/migrations/2023_08_08_145051_create_produtos_table.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id('produto_id');
+            $table->unsignedBigInteger('empresa_id');
+            $table->unsignedBigInteger('grupo_produto_id');
+            $table->unsignedBigInteger('sub_grupo_produto_id');
+            $table->unsignedBigInteger('fornecedor_produto_id');
+            $table->unsignedBigInteger('classe_produto_id');
+            $table->unsignedBigInteger('unidade_id');
+            $table->string('produto_nome', 155);
+            $table->decimal('produto_estoque', 15, 2);
+            $table->decimal('produto_preco', 15, 2);
+            $table->timestamps();
+        });
+
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->foreign('empresa_id')->references('empresa_id')->on('empresas');
+            $table->foreign('grupo_produto_id')->references('grupo_produto_id')->on('grupo_produtos');
+            $table->foreign('sub_grupo_produto_id')->references('sub_grupo_produto_id')->on('sub_grupo_produtos');
+            $table->foreign('fornecedor_produto_id')->references('fornecedor_produto_id')->on('fornecedor_produto');
+            $table->foreign('classe_produto_id')->references('classe_produto_id')->on('classe_produto');
+            $table->foreign('unidade_id')->references('unidade_id')->on('unidades');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('produtos');
+        Schema::table('produtos', function (Blueprint $table) {
+            $table->dropForeign('produtos_empresa_id_foreign');
+            $table->dropForeign('produtos_grupo_produto_id_foreign');
+            $table->dropForeign('produtos_sub_grupo_produto_id_foreign');
+            $table->dropForeign('produtos_fornecedor_produto_id_foreign');
+            $table->dropForeign('produtos_classe_produto_id_foreign');
+            $table->dropForeign('produtos_unidade_id_foreign');
+        });
+    }
+};
