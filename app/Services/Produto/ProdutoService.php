e82e630402b43680db3b519de6455d646daf91ed
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-08 15:24:44 -0400

    feat: criado service ProdutoService

---
+++ app/Services/Produto/ProdutoService.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Services\Produto;
+
+use App\Models\Produto;
+use App\Repositories\Interfaces\Produto\IProduto;
+
+class ProdutoService
+{
+    public function __construct(
+        private readonly IProduto $produtoRepository
+    )
+    {
+
+    }
+
+    public function cadastro(array $produto): Produto
+    {
+        return $this->produtoRepository->cadastro($produto);
+    }
+}
