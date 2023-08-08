498511d5815244eb949072416943e83e3d1df16f
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-08 14:14:43 -0400

    chore: removido a propriedade de resposta para imagens e adicionado para decimais

--- app/Http/Requests/RequestPadroes.php
+++ app/Http/Requests/RequestPadroes.php
@@ -3,12 +3,12 @@
 namespace App\Http\Requests;
 
 class RequestPadroes {
-    public static $string = 'O campo deve receber apenas valores string.';
-    public static $required = 'Esse campo é obrigatório, por favor, informe-o.';
-    public static $email = 'O email informado é inválido.';
-    public static $image = 'O campo deve receber apenas imagens.';
-    public static $integer = 'O campo deve receber apenas inteiros.';
-    public static $array = 'O campo deve receber apenas arrays.';
+    public static string $string = 'O campo deve receber apenas valores string.';
+    public static string $required = 'Esse campo é obrigatório, por favor, informe-o.';
+    public static string $email = 'O email informado é inválido.';
+    public static string $integer = 'O campo deve receber apenas inteiros.';
+    public static string $array = 'O campo deve receber apenas arrays.';
+    public static string $decimal = 'O campo deve receber apenas valores decimais.';
 
     public static function mensagemMax(int $valorMaximo): string {
         return 'Esse campo tem que conter no máximo ' . $valorMaximo . ' caracteres.';
