dbbbdaeff87af7808c24c1a051b8ee30612ac4c2
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-09 17:04:04 -0400

    feat: criado o controller PrazoPgtoController e usado o metodo store para cadastrar novos prazos de pagamentos

---
+++ app/Http/Controllers/PrazoPgtoController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\PrazoPgtoException;
+use App\Services\PrazoPgto\PrazoPgtoService;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PrazoPgtoController extends Controller
+{
+    public function __construct(
+        private readonly PrazoPgtoService $prazoPgtoService
+    )
+    {
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request): JsonResponse
+    {
+        try {
+            $novoPrazoPgto = $this->prazoPgtoService->cadastro($request->only([
+                'prazopgto_nome',
+                'prazopgto_tipo',
+                'prazopgto_tipoforma'
+            ]));
+            return response()->json([
+                'mensagem' => 'O prazo de pagamento foi criado com sucesso',
+                'prazopgto' => $novoPrazoPgto
+            ], Response::HTTP_CREATED);
+        } catch (PrazoPgtoException $ppe) {
+            return response()->json(['error' => $ppe->getMessage()], $ppe->getCode());
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
