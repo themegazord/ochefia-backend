eaa39819e4dbce513cb8e0b14280d3fec55e164f
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-09 14:21:23 -0400

    chore: configurado propriedade casts para lidar com os enums

--- app/Models/PrazoPgto.php
+++ app/Models/PrazoPgto.php
@@ -2,6 +2,8 @@
 
 namespace App\Models;
 
+use App\Enums\Financeiro\PrazoPgto\TiposFormasPrazoPgtoEnum;
+use App\Enums\Financeiro\PrazoPgto\TiposPrazoPgtoEnum;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
@@ -16,6 +18,11 @@ class PrazoPgto extends Model
         'prazopgto_nome',
         'prazopgto_tipo',
         'prazopgto_tipoforma'
+    ];
+
+    protected $casts = [
+        'prazopgto_tipo' => TiposPrazoPgtoEnum::class,
+        'prazopgto_tipoforma' => TiposFormasPrazoPgtoEnum::class
     ];
 
     public function prazoPgtoDias(): HasMany
