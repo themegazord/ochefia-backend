67c224c8f489e1fa8e07831acc0f54cf6f6515a3
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-09 17:02:36 -0400

    feat: criado PrazoPgtoException para gerenciar as exceções correlatos a tabela prazo_pgto e adicionado as validações de tipo e tipoforma que não existe

---
+++ app/Exceptions/PrazoPgtoException.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Exceptions;
+
+use Exception;
+use Symfony\Component\HttpFoundation\Response;
+
+class PrazoPgtoException extends Exception
+{
+    /**
+     * @throws PrazoPgtoException
+     */
+    public static function tipoIncompativelComOsPadroes(string $tipo): self {
+        throw new self('O tipo ' . $tipo . ' não é compativel com os padrões do sistema, insira um válido.', Response::HTTP_CONFLICT);
+    }
+
+    public static function tipoFormaIncompativelComOsPadroes(string $tipoForma): self {
+        throw new self('O tipo de forma de pagamento ' . $tipoForma . ' não é compativel com os padrões do sistema, insira um válido', Response::HTTP_CONFLICT);
+    }
+}
