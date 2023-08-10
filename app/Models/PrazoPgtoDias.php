d95c51b78b30944d26e7a39f747362235b73bc3c
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-10 10:44:10 -0400

    feat: criado o model PrazoPgtoDias e configurado o relacionamento com o model PrazoPgto

---
+++ app/Models/PrazoPgtoDias.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class PrazoPgtoDias extends Model
+{
+    use HasFactory;
+
+    protected $table = 'prazo_pgto_dias';
+
+    protected $fillable = [
+        'prazopgto_id',
+        'parcela',
+        'dias'
+    ];
+
+    public function prazoPgto(): BelongsTo
+    {
+        return $this->belongsTo(PrazoPgto::class, 'prazopgto_id', 'prazopgto_id');
+    }
+}
