25ca636443db4ed1e8e653496ec53498e81f5b56
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-11 16:03:01 -0400

    feat: criado FuncionarioDonoFactory para auxiliar nos testes

---
+++ database/factories/FuncionarioDonoFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Funcionario>
+ */
+class FuncionarioDonoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'empresa_id' => 1,
+            'endereco_id' => 1,
+            'funcionario_nome' => $this->faker->name,
+            'funcionario_email' => $this->faker->email,
+            'funcionario_senha' => 'password',
+            'cargo' => 'Dono',
+            'acessos' => ['*']
+        ];
+    }
+}
