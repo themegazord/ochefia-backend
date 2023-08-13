75bffac16c4152c043b67f8b1e41ee7a52115ee8
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-12 21:36:03 -0400

    chore: adicionado metodo para levantar exception quando tentarem cadastrar um grupo que ja existe

--- app/Exceptions/GrupoProdutoException.php
+++ app/Exceptions/GrupoProdutoException.php
@@ -10,4 +10,7 @@ class GrupoProdutoException extends Exception
     public static function tipoGrupoProdutoNaoExistente(string $tipo): self {
         throw new self('O tipo de produto ' . $tipo . ' não existe no sistema, por favor, verificar na documentação', Response::HTTP_NOT_FOUND);
     }
+    public static function grupoProdutoJaExistente(string $grupoNome): self {
+        throw new self('O grupo ' . strtoupper($grupoNome) . ' já existe na base de dados, cadastre outro ou use-o.', Response::HTTP_CONFLICT);
+    }
 }
