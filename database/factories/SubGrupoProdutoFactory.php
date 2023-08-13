77dfbe526d2a6b05ac86c81a4ff9ecd684a5ae75
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-12 20:59:24 -0400

    feat: criado o SubGrupoProdutoFactory para auxiliar nos testes

---
+++ database/factories/SubGrupoProdutoFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\SubGrupoProduto>
+ */
+class SubGrupoProdutoFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'sub_grupo_produto_nome' => strtoupper($this->faker->word)
+        ];
+    }
+}
