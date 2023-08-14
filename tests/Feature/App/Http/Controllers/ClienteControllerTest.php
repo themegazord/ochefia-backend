1d8d0b881909732e740c6ac528d7a69875ec189b
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-14 11:08:05 -0400

    test: adicionado teste para as questões de cpf com quantidades de digitos diferente de 11 e de todos os numeros serem o mesmo

--- tests/Feature/App/Http/Controllers/ClienteControllerTest.php
+++ tests/Feature/App/Http/Controllers/ClienteControllerTest.php
@@ -97,7 +97,6 @@ class ClienteControllerTest extends TestCase
         $this->assertDatabaseMissing('clientes', $payload);
     }
     public function testLevantarErroAoCadastrarClienteComCPFJaExistente(): void {
-        $usuario = User::factory()->create();
         $cliente = Cliente::factory()->create();
 
         $payload = [
@@ -114,6 +113,42 @@ class ClienteControllerTest extends TestCase
             ->assertJsonStructure(['erro'])
             ->assertJson([
                 'erro' => 'O CPF ' . $payload['cliente_cpf'] . ' já está cadastro, por favor, verifique e tente novamente.'
+            ]);
+    }
+
+    public function testLevantarErroAoCadastrarClienteComCPFComQuantidadeDeDigitosDiferenteDe11(): void {
+        $payload = [
+            'cliente_nome' => $this->faker->name,
+            'cliente_email' => $this->faker->email,
+            'cliente_senha' => 'password',
+            'cliente_cpf' => '0508039160',
+            'cliente_telefone' => $this->faker->phoneNumber,
+            'cliente_telefone_contato' => $this->faker->phoneNumber
+        ];
+
+        $this->post(route('cliente.store'), $payload)
+            ->assertStatus(Response::HTTP_BAD_REQUEST)
+            ->assertJsonStructure(['erro'])
+            ->assertJson([
+                'erro' => 'O CPF ' . $payload['cliente_cpf'] . ' é inválido, por favor, verifique e tente novamente.'
+            ]);
+    }
+
+    public function testLevantarErroAoCadastrarClienteComCPFComTodosOsDigitosIguais(): void {
+        $payload = [
+            'cliente_nome' => $this->faker->name,
+            'cliente_email' => $this->faker->email,
+            'cliente_senha' => 'password',
+            'cliente_cpf' => '00000000000',
+            'cliente_telefone' => $this->faker->phoneNumber,
+            'cliente_telefone_contato' => $this->faker->phoneNumber
+        ];
+
+        $this->post(route('cliente.store'), $payload)
+            ->assertStatus(Response::HTTP_BAD_REQUEST)
+            ->assertJsonStructure(['erro'])
+            ->assertJson([
+                'erro' => 'O CPF ' . $payload['cliente_cpf'] . ' é inválido, por favor, verifique e tente novamente.'
             ]);
     }
 }
