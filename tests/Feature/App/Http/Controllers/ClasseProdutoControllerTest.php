a065ddbe678e458665bb11e0b17c80bc33a82be8
Megazorde <contato.wanjalagus@outlook.com.br>
2023-08-14 08:30:28 -0400

    test: ClasseProdutoControllerTest criado

---
+++ tests/Feature/App/Http/Controllers/ClasseProdutoControllerTest.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Tests\Feature\App\Http\Controllers;
+
+use App\Models\ClasseProduto;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class ClasseProdutoControllerTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+    /**
+     * A basic feature test example.
+     */
+    public function testConseguirCadastrarClasseDeProduto(): void
+    {
+        $usuario = User::factory()->create();
+
+        $payload = [
+            'classe_produto_nome' => $this->faker->word
+        ];
+
+        $this->actingAs($usuario)
+            ->post(route('classe_produto.store'), $payload)
+            ->assertStatus(Response::HTTP_CREATED)
+            ->assertJsonStructure([
+                'mensagem',
+                'classe_produto' => [
+                    'classe_produto_nome',
+                    'id'
+                ]
+            ])
+            ->assertJson([
+                'mensagem' => 'Classe de produtos cadastrado com sucesso',
+                'classe_produto' => [
+                    'classe_produto_nome' => strtoupper($payload['classe_produto_nome']),
+                ]
+            ]);
+        $this->assertDatabaseHas('classe_produto', [
+            'classe_produto_nome' => strtoupper($payload['classe_produto_nome'])
+        ]);
+    }
+
+    public function testLevantarErroDeClasseDeProdutoJaExiste(): void
+    {
+        $usuario = User::factory()->create();
+        $classe = ClasseProduto::factory()->create();
+
+        $payload = [
+            'classe_produto_nome' => $classe->getAttribute('classe_produto_nome')
+        ];
+
+        $this->actingAs($usuario)
+            ->post(route('classe_produto.store'), $payload)
+            ->assertStatus(Response::HTTP_CONFLICT)
+            ->assertJsonStructure(['erro'])
+            ->assertJson([
+                'erro' => 'A classe de produtos ' . strtoupper($payload['classe_produto_nome']) . ' já existe, cadastre outra ou use-a.'
+            ]);
+    }
+}
