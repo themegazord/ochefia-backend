12f6f4634d3434e2efcf827cb90727fb3dc8b69e
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-15 12:40:02 -0400

    test: criado para testar os metodos de FornecedorProdutoService

---
+++ tests/Feature/App/Services/Estoque/Fornecedor/FornecedorProdutoServiceTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\App\Services\Estoque\Fornecedor;
+
+use App\Models\FornecedorProduto;
+use App\Models\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Symfony\Component\HttpFoundation\Response;
+use Tests\TestCase;
+
+class FornecedorProdutoServiceTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+    /**
+     * A basic feature test example.
+     */
+    public function testLevantarErroAoCadastrarFornecedorDeProdutoExistente(): void {
+        $usuario = User::factory()->create();
+        $fornecedor = FornecedorProduto::factory()->create();
+
+        $payload = [
+            'fornecedor_produto_nome' => $fornecedor->getAttribute('fornecedor_produto_nome')
+        ];
+
+        $this->actingAs($usuario)
+            ->post(route('fornecedor_produto.store'), $payload)
+            ->assertStatus(Response::HTTP_CONFLICT)
+            ->assertJsonStructure(['erro'])
+            ->assertJson([
+                'erro' => 'O fornecedor ' . strtoupper($payload['fornecedor_produto_nome']) . ' já existe no banco de dados, por favor, cadastro outro ou use-o'
+            ]);
+    }
+}
