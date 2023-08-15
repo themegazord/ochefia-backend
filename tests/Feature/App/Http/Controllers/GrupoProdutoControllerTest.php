da1f503e74755535aec6c92f51f8c2ff7d1af3fa
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-15 12:37:08 -0400

    test: retirado os testes que não são do controller

--- tests/Feature/App/Http/Controllers/GrupoProdutoControllerTest.php
+++ tests/Feature/App/Http/Controllers/GrupoProdutoControllerTest.php
@@ -180,46 +180,4 @@ class GrupoProdutoControllerTest extends TestCase
         ]);
     }
 
-    public function testLevantarErroDeTipoDeGrupoInexistente(): void
-    {
-        $usuario = User::factory()->create();
-
-        $payload = [
-            'grupo_produto_nome' => 'Despesas',
-            'grupo_produto_tipo' => 'REVENDA'
-        ];
-
-        $this->actingAs($usuario)
-            ->post(route('grupo_produto.store'), $payload)
-            ->assertStatus(Response::HTTP_NOT_FOUND)
-            ->assertJsonStructure(['erro'])
-            ->assertJson([
-                'erro' => 'O tipo de produto ' . $payload['grupo_produto_tipo'] . ' não existe no sistema, por favor, verificar na documentação'
-            ]);
-
-        $this->assertDatabaseMissing('grupo_produtos', [
-            'grupo_produto_nome' => $payload['grupo_produto_nome'],
-            'grupo_produto_tipo' => strtolower($payload['grupo_produto_tipo'])
-        ]);
-    }
-
-    public function testLevantarErroDeGrupoProdutoJaExistente(): void {
-        $usuario = User::factory()->create();
-        $grupo = GrupoProduto::factory()->create();
-
-        $payload = [
-            'grupo_produto_nome' => $grupo->getAttribute('grupo_produto_nome'),
-            'grupo_produto_tipo' => 'PRODUTO_FINAL'
-        ];
-
-        $this->actingAs($usuario)
-            ->post(route('grupo_produto.store'), $payload)
-            ->assertStatus(Response::HTTP_CONFLICT)
-            ->assertJsonStructure([
-                'erro'
-            ])
-            ->assertJson([
-                'erro' => 'O grupo ' . strtoupper($payload['grupo_produto_nome']) . ' já existe na base de dados, cadastre outro ou use-o.'
-            ]);
-    }
 }
