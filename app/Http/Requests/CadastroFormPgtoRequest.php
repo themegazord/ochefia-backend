dd6c21256afdbfba59d6c3dec6064de3d5c0f385
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-08-17 13:32:51 -0400

    feat: criado request CadastroFormaPgtoRequest para validar os dados do cadastro de forma de pagamento

---
+++ app/Http/Requests/CadastroFormPgtoRequest.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CadastroFormPgtoRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'formapgto_nome' => 'required|string|max:50',
+            'formapgto_tipo' => 'required|string|max:30',
+            'clientes_id' => 'integer|exists:clientes,clientes_id',
+            'prazopgto_id' => 'integer|exists:prazo_pgto,prazopgto_id'
+        ];
+    }
+
+    public function messages(): array
+    {
+        return [
+            'required' => RequestPadroes::$required,
+            'string' => RequestPadroes::$string,
+            'integer' => RequestPadroes::$integer,
+            'clientes_id.exists' => RequestPadroes::mensagemExists('clientes'),
+            'prazopgto_id.exists' => RequestPadroes::mensagemExists('prazo de pagamento'),
+            'formapgto_nome.max' => RequestPadroes::mensagemMax(50),
+            'formapgto_tipo.max' => RequestPadroes::mensagemMax(30)
+        ];
+    }
+}
