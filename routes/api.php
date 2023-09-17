e56308b855cca1e1a517e6e4cfbc2c24dccbdcf4
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-09-17 09:56:49 -0400

    chore: adicionado rota de edição de grupo de produto

--- routes/api.php
+++ routes/api.php
@@ -51,6 +51,7 @@ Route::prefix('v1')->group(function() {
             Route::post('cadastro', [GrupoProdutoController::class, 'store'])->name('grupo_produto.store');
             Route::get('listagem', [GrupoProdutoController::class, 'index'])->name('grupo_produto.index');
             Route::get('grupo/{id}', [GrupoProdutoController::class, 'show'])->name('grupo_produto.show');
+            Route::put('grupo/{id}', [GrupoProdutoController::class, 'update'])->name('grupo_produto.update');
         });
         Route::prefix('sub_grupo_produto')->group(function() {
             Route::post('cadastro', [SubGrupoProdutoController::class, 'store'])->name('sub_grupo_produto.store');
