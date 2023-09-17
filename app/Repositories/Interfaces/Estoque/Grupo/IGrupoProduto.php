f5bf6337a240c003b9e9ade7153c0e4744ec6b19
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-09-17 09:59:14 -0400

    chore: adicionado metodo edicaoGrupoPorId no contrato

--- app/Repositories/Interfaces/Estoque/Grupo/IGrupoProduto.php
+++ app/Repositories/Interfaces/Estoque/Grupo/IGrupoProduto.php
@@ -3,13 +3,16 @@
 namespace App\Repositories\Interfaces\Estoque\Grupo;
 
 use App\Models\GrupoProduto;
+use Illuminate\Database\Eloquent\Collection;
 
 interface IGrupoProduto
 {
     public function cadastro(array $grupoProduto): GrupoProduto;
     public function grupoProdutoPorNome(string $grupoNome): ?GrupoProduto;
 
-    public function listagemTotalGrupo(): \Illuminate\Database\Eloquent\Collection;
+    public function listagemTotalGrupo(): Collection;
 
-    public function grupoPorId(int $id): \Illuminate\Database\Eloquent\Collection;
+    public function grupoPorId(int $id): Collection;
+
+    public function edicaoGrupoPorId(array $grupo, int $id): int;
 }
