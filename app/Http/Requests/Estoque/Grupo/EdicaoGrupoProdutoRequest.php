8e123a67e25f1b4afde32dc133d9b2c5cd122828
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-09-18 14:42:19 -0400

    chore: request movido para dentro da devida pasta

--- app/Http/Requests/Estoque/Grupo/EdicaoGrupoProdutoRequest.php
+++ app/Http/Requests/Estoque/Grupo/EdicaoGrupoProdutoRequest.php
@@ -1,7 +1,8 @@
 <?php
 
-namespace App\Http\Requests;
+namespace App\Http\Requests\Estoque\Grupo;
 
+use App\Http\Requests\RequestPadroes;
 use Illuminate\Foundation\Http\FormRequest;
 
 class EdicaoGrupoProdutoRequest extends FormRequest
