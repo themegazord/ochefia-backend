cc900fae7586649e079485e8631bd3bdd99efe9f
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-09-18 16:40:21 -0400

    chore: implementado metodo de listagem de classes do contrato no repository

--- app/Repositories/Eloquent/Estoque/Classe/ClasseProdutoRepository.php
+++ app/Repositories/Eloquent/Estoque/Classe/ClasseProdutoRepository.php
@@ -4,6 +4,7 @@ namespace App\Repositories\Eloquent\Estoque\Classe;
 
 use App\Models\ClasseProduto;
 use App\Repositories\Interfaces\Estoque\Classe\IClasseProduto;
+use Illuminate\Database\Eloquent\Collection;
 
 class ClasseProdutoRepository implements IClasseProduto
 {
@@ -19,5 +20,14 @@ class ClasseProdutoRepository implements IClasseProduto
         return ClasseProduto::query()
             ->where('classe_produto_nome', $nomeClasseProduto)
             ->first();
+    }
+
+    public function listagemClasseProduto(): Collection
+    {
+        return ClasseProduto::query()
+            ->get([
+                'classe_produto_id',
+                'classe_produto_nome'
+            ]);
     }
 }
