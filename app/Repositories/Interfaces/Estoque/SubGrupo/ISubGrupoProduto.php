bb36ef11ded4b20b3e5ede09c3fc1604fba81936
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-09-18 00:01:33 -0400

    chore: adicionado metodo no contrato responsavel por retornar lista de subgrupos

--- app/Repositories/Interfaces/Estoque/SubGrupo/ISubGrupoProduto.php
+++ app/Repositories/Interfaces/Estoque/SubGrupo/ISubGrupoProduto.php
@@ -3,9 +3,11 @@
 namespace App\Repositories\Interfaces\Estoque\SubGrupo;
 
 use App\Models\SubGrupoProduto;
+use Illuminate\Database\Eloquent\Collection;
 
 interface ISubGrupoProduto
 {
     public function cadastro(array $subGrupo): SubGrupoProduto;
     public function subGrupoPorNome(string $nomeSubGrupo): ?SubGrupoProduto;
+    public function listagem(): Collection;
 }
