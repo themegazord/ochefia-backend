fdfe1e9752a519b97b44f57cd750466eadb7baf9
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-09-18 16:39:52 -0400

    chore: adicionado metodo de listagem dentro do contrato

--- app/Repositories/Interfaces/Estoque/Classe/IClasseProduto.php
+++ app/Repositories/Interfaces/Estoque/Classe/IClasseProduto.php
@@ -3,10 +3,11 @@
 namespace App\Repositories\Interfaces\Estoque\Classe;
 
 use App\Models\ClasseProduto;
+use Illuminate\Database\Eloquent\Collection;
 
 interface IClasseProduto
 {
     public function cadastro(array $classeProduto): ClasseProduto;
-
     public function classeProdutoPorNome(string $nomeClasseProduto): ?ClasseProduto;
+    public function listagemClasseProduto(): Collection;
 }
