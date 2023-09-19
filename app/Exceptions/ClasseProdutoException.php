6d29db3f076c2e99ef850524284b48f8710feb2d
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-09-19 15:18:01 -0400

    chore: adicionado exceção para classes inexistentes

--- app/Exceptions/ClasseProdutoException.php
+++ app/Exceptions/ClasseProdutoException.php
@@ -14,4 +14,11 @@ class ClasseProdutoException extends Exception
     {
         throw new self('A classe de produtos ' . strtoupper($nomeClasseProduto) . ' já existe, cadastre outra ou use-a.', Response::HTTP_CONFLICT);
     }
+
+    /**
+     * @throws ClasseProdutoException
+     */
+    public static function classeInexistente(): self {
+        throw new self('A classe não existe', Response::HTTP_NOT_FOUND);
+    }
 }
