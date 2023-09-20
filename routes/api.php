24ff0c758bcd2db98fb5ed696c5b87304a7191dd
Gustavo de Camargo Campos <contato.wanjalagus@outlook.com.br>
2023-09-20 14:24:19 -0400

    chore: adicionado rota para listagem de fabricantes

--- routes/api.php
+++ routes/api.php
@@ -63,6 +63,7 @@ Route::prefix('v1')->group(function() {
         });
         Route::prefix('fabricante_produto')->group(function() {
             Route::post('cadastro', [FabricanteProdutoController::class, 'store'])->name('fabricante_produto.store');
+            Route::get('listagem', [FabricanteProdutoController::class, 'index'])->name('fabricante_produto.index');
         });
         Route::prefix('unidade')->group(function() {
            Route::post('cadastro', [UnidadeController::class, 'store'])->name('unidade.store');
